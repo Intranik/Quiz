5d6e84a5f6d6c337ffe0630aede18671103381c9
intranik <intranik.khazro@hotmail.com>
2024-12-16 00:03:42 +0100

    dark mode and light mode

---
+++ quiz.js
@@ -0,0 +1,24 @@
+'use strict';
+const modeBtn = document.getElementById("mode");
+const backgr = document.querySelector(".background");
+const video= document.querySelector(".video-bg");
+const newGameBtn = document.getElementById("new_Game");
+const nexBtn = document.getElementById("next");
+const backBtn = document.getElementById("back");
+//Mode button Dark / Light
+modeBtn.addEventListener('click', ()=>{
+    document.body.classList.toggle("background-black");
+    
+    if (document.body.classList.contains("background-black")) {
+        video.classList.remove("video-bg-hidden");
+        modeBtn.textContent = "Light";
+        document.body.classList.add("dark-mode");
+        document.body.classList.remove("light-mode");
+    } else {
+        video.classList.add("video-bg-hidden");
+        modeBtn.textContent = "Dark";
+        document.body.classList.add("light-mode");
+        document.body.classList.remove("dark-mode");
+    }
+
+});
